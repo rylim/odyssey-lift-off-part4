433c6688dbd98c75c65831d000955f2503c2cf20
rylim <36293596+rylim@users.noreply.github.com>
2022-12-31 18:20:19 -0800

    Liftoff Part 4

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -24,6 +24,28 @@ const resolvers = {
       return dataSources.trackAPI.getTrackModules(id);
     },
   },
+  Mutation: {
+    // increments a track's numberOfViews property
+    incrementTrackViews: async (_, {id}, {dataSources}) => {
+      try {
+        // where we'll call the TrackAPI
+        const track = await dataSources.trackAPI.incrementTrackViews(id);
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track
+        };
+      } catch (err) {
+        return {
+          code: err.extensions.response.status,
+          success: false,
+          message: err.extensions.response.body,
+          track: null
+        };
+      }
+    },
+  },
 };
 
 module.exports = resolvers;
